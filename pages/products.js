01002c9a500e51dd75ad7293e22e2fef3af71228
Florin Gologan <florin.gologan@made.com>
2020-06-19 12:29:35 +0300

    Load products by calling the api

--- pages/products.js
+++ pages/products.js
@@ -1,11 +1,35 @@
-import Layout from '../components/Layout';
+import { Component } from 'react';
 import Head from 'next/head';
+import axios from 'axios';
+import Layout from '../components/Layout';
 
-export default () => (
-  <Layout>
-    <Head>
-      <title>SSR - Products Page</title>
-    </Head>
-    <h1>Products Page</h1>
-  </Layout>
-)
+export default class Products extends Component {
+  static async getInitialProps () {
+    const res = await axios.get('http://localhost:3000/api/products.json');
+
+    return {products: res.data};
+  }
+
+  renderProducts () {
+    const {products = []} = this.props;
+
+    return products.map(product => (
+        <li key={ product.id }>{ product.name }</li>
+      )
+    );
+  }
+
+  render () {
+    return (
+      <Layout>
+        <Head>
+          <title>SSR - Products Page</title>
+        </Head>
+        <h1>Products Page</h1>
+        <ul>
+          { this.renderProducts() }
+        </ul>
+      </Layout>
+    );
+  }
+}
